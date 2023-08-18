b1e88c70a9d5c063abb68a351cb1511622f045e4
ayub <ajibade.ayuba@gmail.com>
2023-08-18 03:56:39 -0700

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h" /* Include the header file where _putchar is declared */
+
+/**
+ * print_diagonal - Draws a diagonal line using the \ character
+ * @n: The number of times the \ character should be printed
+ */
+void print_diagonal(int n)
+{
+	int spaces = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n'); /* Print a newline if n is 0 or less */
+		return;
+	}
+
+	while (n > 0)
+	{
+		/* Print leading spaces */
+		for (int i = 0; i < spaces; i++)
+			_putchar(' ');
+
+		_putchar('\\'); /* Print the \ character */
+		_putchar('\n'); /* Print a newline after each diagonal character */
+
+		spaces++; /* Increase the number of leading spaces for the next iteration */
+		n--;
+	}
+}

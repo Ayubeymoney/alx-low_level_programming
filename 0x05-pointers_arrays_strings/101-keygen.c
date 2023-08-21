5c906d1bdeb89179484a3ef5281907b7e1eee64b
ayub <ajibade.ayuba@gmail.com>
2023-08-21 14:16:34 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,21 +11,16 @@
 int main(void)
 {
 	int i;
-	char password[12]; /* The password length crackme is 11 characters */
-	char expected_output[] = "Tada! Congrats"; /* Expected output */
+	char password[12];
 
-	srand(time(0)); /* Seed the random  with the current time */
+	srand(time(0));
 
-	/*Keep generating passwords until the expected output is achieved */
-	do {
-		for (i = 0; i < 11; i++)
-		{
-			/* Generate a ASCII range of valid characters */
-			password[i] = rand() % 94 + 32; /* ASCII range: 32 to 125 */
-		}
+	for (i = 0; i < 11; i++)
+	{
+		password[i] = rand() % 94 + 32;
+	}
 
-		password[11] = '\0'; /* Null-terminate the string */
-	} while (strcmp(password, expected_output) != 0);
+	password[11] = '\0';
 
 	printf("Generated password: %s\n", password);
 

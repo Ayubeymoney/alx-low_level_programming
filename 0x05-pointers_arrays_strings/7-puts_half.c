82ed3a323a7e1fb4defa0b0986641611b4de2bfb
ayub <ajibade.ayuba@gmail.com>
2023-08-21 10:21:47 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints the second half of a string.
+ * @str: The string to be processed.
+ */
+void puts_half(char *str)
+{
+	int length = 0;
+	int i, start;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	start = (length + 1) / 2;
+
+	for (i = start; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}

58994daf4745e0d1421320d0843a6156bc03aa32
ayub <ajibade.ayuba@gmail.com>
2023-09-11 08:43:51 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,7 @@
- /**
+#ifndef DOG_H
+#define DOG_H
+
+/**
  * struct dog - Structure to represent information about a dog
  * @name: The name of the dog (string)
  * @age: The age of the dog (float)
@@ -13,3 +16,5 @@ struct dog
 	float age;
 	char *owner;
 };
+
+#endif /* DOG_H */
